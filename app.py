396b45cb939dcb03e1acbf8b55fb8bb9602cd8cc
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-04-24 18:18:34 -0300

    feat(app): add route that allows users to logout

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from flask_login import LoginManager, UserMixin, login_required, login_user
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -50,6 +50,14 @@ def login():
         return jsonify({'message': 'Logged in successfully!'})
 
     return jsonify({'message': 'Unauthorized! Invalid credentials!'}), 401
+
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+
+    return jsonify({'message': 'Logged out successfully!'})
 
 
 @app.route('/api/products/add', methods=["POST"])
